42fd7604d5d948e342b769bbad3fe62d41a10983
chad luo <i@yuki.rocks>
2026-02-21 10:01:36 +1100

    remove unused file

--- content.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-// content.js
-// This script will be injected into the web pages
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Organic New Tab Page content script loaded.");
-});
